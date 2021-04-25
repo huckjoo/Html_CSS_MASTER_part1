32a1114efc3ec75349f07d1dfeec046224d3f978
huckjoo <huckjoo96@naver.com>
2021-04-25 20:51:30 +0900

    update slider

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const bar = document.querySelector('.nav__hamburger');
 const menu = document.querySelector('.nav__menus');
+const firstSlide = document.querySelector('.main__slider__box:first-child');
 bar.addEventListener('click',function(){
     menu.classList.toggle('active');
 });
+firstSlide.classList.add("showing");
+// let changeSlide = function(){
+//     if(firstSlide.hasClass('showing')){
+//         firstSlide.classList.delete("showing");
+//         firstSlide.nextSibling.classList.add('showing');
+//     }
+// }
+// changeSlide();
